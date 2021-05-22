5250d7898afa0fdbd8f0adbffd390963d22a4bcb
kawarimidoll <kawarimidoll+git@gmail.com>
2021-05-22 21:53:38 +0900

    update widget from stateless to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -16,7 +17,14 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 3;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -24,22 +32,39 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: TextButton(
-              child: Image.asset('images/dice1.png'),
               onPressed: () {
+                setState(() {
+                  changeDiceFace();
+                });
                 print('Left button got pressed!');
               },
+              child: Image.asset('images/dice$leftDiceNum.png'),
             ),
           ),
           Expanded(
             child: TextButton(
-              child: Image.asset('images/dice1.png'),
               onPressed: () {
+                setState(() {
+                  changeDiceFace();
+                });
                 print('Right button got pressed!');
               },
+              child: Image.asset('images/dice$rightDiceNum.png'),
             ),
           ),
         ],
       ),
     );
+  }
+
+  void changeDiceFace(){
+    leftDiceNum = rollDice();
+    rightDiceNum = rollDice();
+    print('Left dice: $leftDiceNum');
+    print('Right dice: $rightDiceNum');
+  }
+
+  int rollDice() {
+    return Random().nextInt(6) + 1;
   }
 }
